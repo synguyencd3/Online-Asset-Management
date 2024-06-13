5910cd2e28208812cf83db374ecde489a6a64a3d
tragiangphan.work <tragiangphan.work@gmail.com>
2024-06-13 07:48:56 +0000

    Merged PR 615: Create color constants
    Create color constants

--- src/components/commons/HeaderComponent.tsx
+++ src/components/commons/HeaderComponent.tsx
@@ -3,7 +3,7 @@ import { Navbar, Container } from 'react-bootstrap'
 
 export const HeaderComponent: React.FC<{ title: string }> = ({ title }) => {
   return (
-    <Navbar className="navbar" style={{ backgroundColor: '#cf2338' }}>
+    <Navbar className="navbar" style={{ backgroundColor: '#cf2338', height: '90px' }}>
       <Container>
         <Navbar.Brand href="#" className='d-flex align-items-center'>
           <h3 className="mx-5 my-2" style={{ color: '#ffffff', fontWeight: '700' }}>{title}</h3>
