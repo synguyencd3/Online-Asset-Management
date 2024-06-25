28791a62a152b305bbb5742903e8928757475ba3
tragiangphan <tragiangphan.work@gmail.com>
2024-06-25 17:01:12 +0700

    fixed to deploy

--- src/components/admin/user/ManageUserComponent.tsx
+++ src/components/admin/user/ManageUserComponent.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Dropdown, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { TableComponent } from "../../commons/TableComponent";
 import { DropdownFilterComponent } from "../../commons/DropdownFilterComponent";
 import { SearchComponent } from "../../commons/SearchComponent";
